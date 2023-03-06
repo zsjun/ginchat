9641e30d2b07d31a091d9e783b03a793dc3e3bbc
zsj <zsoujun@126.com>
2023-03-06 22:41:10 +0800

    add cookie and session

--- go.mod
+++ go.mod
@@ -14,6 +14,9 @@ require (
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/spec v0.20.8 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -30,8 +33,9 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
-	github.com/bytedance/sonic v1.8.2 // indirect
+	github.com/bytedance/sonic v1.8.3 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.0
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -53,11 +57,11 @@ require (
 	github.com/swaggo/gin-swagger v1.5.3
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.10 // indirect
-	golang.org/x/arch v0.2.0 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.4.7
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"ginchat/router"
 	"ginchat/utils"
 	"net/http"
-	"time"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -20,26 +20,28 @@ type User struct {
 
 func getCurrentUser(c *gin.Context) (userInfo User) {
 	session := sessions.Default(c)
-	userInfo = session.Get("currentUser").(User) // 类型转换一下
+  // 类型转换一下
+	userInfo = session.Get("currentUser").(User) 
 	return
 }
 
 func setCurrentUser(c *gin.Context, userInfo User) {
 	session := sessions.Default(c)
 	session.Set("currentUser", userInfo)
-        // 一定要Save否则不生效，若未使用gob注册User结构体，调用Save时会返回一个Error
+  // 一定要Save否则不生效，若未使用gob注册User结构体，调用Save时会返回一个Error
 	session.Save() 
 }
 
 func setupRouter(r *gin.Engine) {
 	r.POST("/login", func(c *gin.Context) {
-		var loginVo User
-		if c.ShouldBindJSON(&loginVo) != nil {
+		loginUserInfo := User{}
+		if c.ShouldBindJSON(&loginUserInfo) != nil {
 			c.String(http.StatusOK, "参数错误")
 			return
 		}
-		if loginVo.Email == db.Email && loginVo.Password == db.Password {
-			setCurrentUser(c, *db) // 邮箱和密码正确则将当前用户信息写入session中
+		if loginUserInfo.Email == db.Email && loginUserInfo.Password == db.Password {
+      // 邮箱和密码正确则将当前用户信息写入session中
+			setCurrentUser(c, *db)
 			c.String(http.StatusOK, "登录成功")
 		} else {
 			c.String(http.StatusOK, "登录失败")
@@ -48,27 +50,29 @@ func setupRouter(r *gin.Engine) {
 
 	r.GET("/sayHello", func(c *gin.Context) {
 		userInfo := getCurrentUser(c)
-		c.String(http.StatusOK, "Hello "+userInfo.Name)
+		c.String(http.StatusOK, "Hello "+userInfo.Username)
 	})
 }
-
-var db = &User{Id: 10001, Email: "abc@gmail.cn", Username: "Alice", Password: "123456"} // 不操作数据库，把所有用户信息写死在代码里
+// 不操作数据库，把所有用户信息写死在代码里
+var db = &User{Id: 10001, Email: "abc@gmail.cn", Username: "Alice", Password: "123456"} 
 
 
 
 func main() {
-  
+  // 注册User结构体
+  gob.Register(User{}) 
   utils.InitConfig()
   utils.InitMysql()
   r := router.Router()
   fmt.Println("hello, world1")
-
-  r.GET("/login", Login)
-
   // 设置生成sessionId的密钥
   store := cookie.NewStore([]byte("secret")) 
   // mysession是返回給前端的sessionId名
   r.Use(sessions.Sessions("mysession", store))
+
+  setupRouter(r)
+
+  
 
   r.GET("/hello", func(c *gin.Context) {
     session := sessions.Default(c)
@@ -83,62 +87,62 @@ func main() {
 
 
   // 需要登陆保护的
-  auth := r.Group("/api")
-	auth.Use(AuthRequired())
-	{
-		auth.GET("/me", UserInfo)
-		auth.GET("/logout", Logout)
-	}
+  // auth := r.Group("/api")
+	// auth.Use(AuthRequired())
+	// {
+	// 	auth.GET("/me", UserInfo)
+	// 	auth.GET("/logout", Logout)
+	// }
   r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 // 登陆
-func Login(c *gin.Context) {
-  // 为了演示方便，我直接通过url明文传递账号密码，实际生产中应该用HTTP POST在body中传递
-  userID := c.Query("user_id")
-  password := c.Query("password")
-  fmt.Println(userID,password)
-  // 用户身份校验（查询数据库）
-  if userID == "007" && password == "007" {
-    // 生成cookie
-    expiration := time.Now()
-    expiration = expiration.AddDate(0, 0, 1)
-    // 实际生产中我们可以加密userID
-    cookie := http.Cookie{Name: "userID", Value: userID, Expires: expiration}
-    http.SetCookie(c.Writer, &cookie)
+// func Login(c *gin.Context) {
+//   // 为了演示方便，我直接通过url明文传递账号密码，实际生产中应该用HTTP POST在body中传递
+//   userID := c.Query("user_id")
+//   password := c.Query("password")
+//   fmt.Println(userID,password)
+//   // 用户身份校验（查询数据库）
+//   if userID == "007" && password == "007" {
+//     // 生成cookie
+//     expiration := time.Now()
+//     expiration = expiration.AddDate(0, 0, 1)
+//     // 实际生产中我们可以加密userID
+//     cookie := http.Cookie{Name: "userID", Value: userID, Expires: expiration}
+//     http.SetCookie(c.Writer, &cookie)
 
-    c.JSON(http.StatusOK, gin.H{"msg": "Hello " + userID})
-    return
-  }else {
-    c.JSON(http.StatusBadRequest, gin.H{"msg": "账号或密码错误"})
-  }
-}
+//     c.JSON(http.StatusOK, gin.H{"msg": "Hello " + userID})
+//     return
+//   }else {
+//     c.JSON(http.StatusBadRequest, gin.H{"msg": "账号或密码错误"})
+//   }
+// }
 
-// 检测是否登陆的中间件
-func AuthRequired() gin.HandlerFunc {
-  return func(c *gin.Context) {
-    cookie, _ := c.Request.Cookie("userID")
-    if cookie == nil {
-      c.JSON(http.StatusUnauthorized, gin.H{"msg": "请先登陆"})
-      c.Abort()
-    }
-    // 实际生产中应校验cookie是否合法
-    c.Next()
-  }
-}
+// // 检测是否登陆的中间件
+// func AuthRequired() gin.HandlerFunc {
+//   return func(c *gin.Context) {
+//     cookie, _ := c.Request.Cookie("userID")
+//     if cookie == nil {
+//       c.JSON(http.StatusUnauthorized, gin.H{"msg": "请先登陆"})
+//       c.Abort()
+//     }
+//     // 实际生产中应校验cookie是否合法
+//     c.Next()
+//   }
+// }
 
-// 查看用户个人信息
-func UserInfo(c *gin.Context) {
-  c.JSON(http.StatusOK, gin.H{"msg": "007的个人页面"})
-}
+// // 查看用户个人信息
+// func UserInfo(c *gin.Context) {
+//   c.JSON(http.StatusOK, gin.H{"msg": "007的个人页面"})
+// }
 
-// 退出登陆
-func Logout(c *gin.Context) {
-  // 设置cookie过期
-  expiration := time.Now()
-  expiration = expiration.AddDate(0, 0, -1)
-  cookie := http.Cookie{Name: "userID", Value: "", Expires: expiration}
-  http.SetCookie(c.Writer, &cookie)
+// // 退出登陆
+// func Logout(c *gin.Context) {
+//   // 设置cookie过期
+//   expiration := time.Now()
+//   expiration = expiration.AddDate(0, 0, -1)
+//   cookie := http.Cookie{Name: "userID", Value: "", Expires: expiration}
+//   http.SetCookie(c.Writer, &cookie)
 
-  c.JSON(http.StatusOK, gin.H{"msg": "退出成功"})
-}
+//   c.JSON(http.StatusOK, gin.H{"msg": "退出成功"})
+// }
