d89c3fade598d869c4d496710f8412291977988e
zsj <zousjun@126.com>
2023-03-08 10:05:52 +0800

    add upload api

--- router/router.go
+++ router/router.go
@@ -1,10 +1,13 @@
 package router
 
 import (
+	"fmt"
 	"ginchat/common"
 	"ginchat/docs"
 	"ginchat/service"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -72,6 +75,32 @@ func Router() *gin.Engine {
 			"message": message,
 			"nick":    nick,
 		})
+	})
+
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	r.MaxMultipartMemory = 50 << 20 // 50 MiB
+	r.POST("/upload", func(c *gin.Context) {
+		// Single file
+		file, err := c.FormFile("file")
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		distPath := "uploads/"
+		if _, err := os.Stat(distPath); os.IsNotExist(err) {
+			os.Mkdir(distPath, os.ModePerm)
+		}
+
+		dst := filepath.Join(distPath, file.Filename)
+		err = c.SaveUploadedFile(file, dst)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
 
 	return r
